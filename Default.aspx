c28181279a239145627676ad2ec70ec89769711a
unknown <sergio.luis.huanca@gmail.com>
2021-12-15 07:26:10 -0400

    primera version

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        CALCULADORA EN F#</div>
+        Numero 1:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<p>
+            Numero 2:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            resultado:<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SUMAR" Width="135px" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="RESTAR" Width="137px" />
+        <p>
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="MULTIPLICAR" Width="131px" />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="DIVIDIR" Width="143px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r = PortableLibrary3.MiModulo.suma(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r = PortableLibrary3.MiModulo.resta(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r = PortableLibrary3.MiModulo.multi(a, b);
+        TextBox3.Text = r.ToString();
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(TextBox1.Text);
+        int b = Convert.ToInt32(TextBox2.Text);
+        int r = PortableLibrary3.MiModulo.divi(a, b);
+        TextBox3.Text = r.ToString();
+    }
+}
